405d374b206fabf1913962517ddf3221d2ab3353
viery <viery@NBMHQA035s-MacBook-Pro.local>
2023-09-28 11:23:05 +0700

    inital commit

---
+++ resources/views/layouts/partials/js.blade.php
@@ -0,0 +1,13 @@
+<script src="{{ asset('assets/js/helpers.js') }}"></script>
+<script src="{{ asset('assets/js/config.js') }}"></script>
+
+<script src="{{ asset('assets/js/jquery.js') }}"></script>
+<script src="{{ asset('assets/js/popper.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+<script src="{{ asset('assets/js/perfect-scrollbar.js') }}"></script>
+
+<script src="{{ asset('assets/js/hammer.js') }}"></script>
+
+<script src="{{ asset('assets/js/menu.js') }}"></script>
+
+<script src="{{ asset('assets/js/main.js') }}"></script>
---
+++ resources/views/layouts/partials/menu.blade.php
@@ -0,0 +1,39 @@
+<!-- Menu -->
+<aside id="layout-menu" class="layout-menu-horizontal menu-horizontal menu bg-menu-theme flex-grow-0">
+    <div class="container-xxl d-flex h-100">
+        <ul class="menu-inner py-1">
+            <!-- Page -->
+            <li class="menu-item active">
+                <a href="#" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                    Dashboard
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-detail"></i>
+                    Page 2
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-detail"></i>
+                    Page 3
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-detail"></i>
+                    Page 4
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-detail"></i>
+                    Page 5
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
+<!-- / Menu -->
