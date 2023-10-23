553c11c038f0f4969974039127dd9be54d6e40aa
viery <vierydarmawan2@gmail.com>
2023-10-23 19:58:19 +0700

    show image logo in column

--- app/DataTables/CarBrandDataTable.php
+++ app/DataTables/CarBrandDataTable.php
@@ -4,15 +4,26 @@ namespace App\DataTables;
 
 use App\Models\CarBrand;
 use Yajra\DataTables\Services\DataTable;
+use App\Services\UploadService;
 
 class CarBrandDataTable extends DataTable
 {
+    protected $uploadService;
+
+    public function __construct(UploadService $uploadService)
+    {
+        $this->uploadService = $uploadService;
+    }
+
     public function dataTable($query)
     {
         return datatables()
             ->eloquent($query)
-            ->addColumn('action', 'path.to.action.view')
-            ->rawColumns(['action']);
+            ->addColumn('action', 'car.car-brand-action')
+            ->addColumn('logo', function (CarBrand $carBrand) {
+                return '<img src="' . $this->uploadService->getPublicUrl($carBrand->logo) . '" width="100">';
+            })
+            ->rawColumns(['action', 'logo']);
     }
 
     public function query(CarBrand $model)
---
+++ resources/views/car/car-brand-action.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <button class="btn btn-sm btn-warning"><span class="fa fa-pencil"></span> Edit</button>
+    <button class="btn btn-sm btn-danger"><span class="fa fa-trash"></span> Delete</button>
+</div>
